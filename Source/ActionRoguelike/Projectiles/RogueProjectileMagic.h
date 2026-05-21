d110936506852b63d4046c8b5cbd853f65498614
Jason Platte <jasonplatte@hotmail.com>
2026-05-21 15:59:11 -0700

    Add Projectile Effect

--- Source/ActionRoguelike/Projectiles/RogueProjectileMagic.h
+++ Source/ActionRoguelike/Projectiles/RogueProjectileMagic.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "RogueProjectileMagic.generated.h"
 
+class UNiagaraSystem;
 class UNiagaraComponent;
 class UProjectileMovementComponent;
 class USphereComponent;
@@ -16,16 +17,23 @@ class ACTIONROGUELIKE_API ARogueProjectileMagic : public AActor
 	GENERATED_BODY()
 
 protected:
-	UPROPERTY(EditDefaultsOnly, Category = "Components");
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	TObjectPtr<UNiagaraSystem> ExplosionEffect;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
 	TObjectPtr<USphereComponent> SphereComponent;
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Components");
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
 	TObjectPtr<UNiagaraComponent> LoopedNiagaraComponent;
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Components");
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
 	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
 	
+	UFUNCTION()
+	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
 public:
+	virtual void PostInitializeComponents() override;
 	// Sets default values for this actor's properties
 	ARogueProjectileMagic();
 
