8e4cc0bd7f9f14807aec90fc2ea56a2f19ad4983
Jason Platte <jasonplatte@hotmail.com>
2026-05-20 18:03:57 -0700

    Add Projectile Attack

---
+++ Source/ActionRoguelike/Projectiles/RogueProjectileMagic.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueProjectileMagic.generated.h"
+
+class UNiagaraComponent;
+class UProjectileMovementComponent;
+class USphereComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogueProjectileMagic : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Components");
+	TObjectPtr<USphereComponent> SphereComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components");
+	TObjectPtr<UNiagaraComponent> LoopedNiagaraComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components");
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+	
+public:
+	// Sets default values for this actor's properties
+	ARogueProjectileMagic();
+
+};
